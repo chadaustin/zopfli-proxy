66c19af8e276f64dd1cf71a4ba88c5d79ed11553
Chad Austin <caustin@gmail.com>
2014-02-11 02:48:08 -0800

    start talking to backend

--- main.hs
+++ main.hs
@@ -8,18 +8,23 @@ import Foreign.Marshal.Alloc
 
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Network.Wai as Wai
+import Network.HTTP
+import Network.HTTP.Base
 import qualified Network.HTTP.Types as HT
 import qualified Network.Wai.Handler.Warp as Warp
 
 foreign import ccall "zopfli_compress" zopfli_compress
   :: CSize -> CString -> Ptr CSize -> CString
 
-app _request = do
-    resp <- alloca $ \output_size -> return $ BSL.pack $ show $ zopfli_compress (CSize 0) nullPtr output_size
-    return $ Wai.responseLBS HT.status200 [] resp
+app config _request = do
+  backend <- simpleHTTP (getRequest "http://localhost:4000")
+  
+  resp <- alloca $ \output_size -> return $ BSL.pack $ show $ zopfli_compress (CSize 0) nullPtr output_size
+  return $ Wai.responseLBS HT.status200 [] resp
 
 main :: IO ()
 main = do
-    Warp.run 3000 app
+  let config = "http://localhost:4000"
+  Warp.run 3000 $ app config
 
-    return ()
+  return ()
