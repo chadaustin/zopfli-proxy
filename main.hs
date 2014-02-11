d0a5e0399dc25f76c921db44542ae0edf4b559c8
Chad Austin <caustin@gmail.com>
2014-02-11 02:23:23 -0800

    checkpoint haskell calling into C

--- main.hs
+++ main.hs
@@ -2,15 +2,20 @@
 module Main where
 
 import Control.Monad.IO.Class
+import Foreign.C
+import Foreign.Ptr
+import Foreign.Marshal.Alloc
 
+import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Network.Wai as Wai
 import qualified Network.HTTP.Types as HT
 import qualified Network.Wai.Handler.Warp as Warp
 
-foreign import 
+foreign import ccall "zopfli_compress" zopfli_compress
+  :: CSize -> CString -> Ptr CSize -> CString
 
 app _request = do
-    let resp = "PONG"
+    resp <- alloca $ \output_size -> return $ BSL.pack $ show $ zopfli_compress (CSize 0) nullPtr output_size
     return $ Wai.responseLBS HT.status200 [] resp
 
 main :: IO ()
---
+++ zopfli_compress.c
@@ -0,0 +1,6 @@
+#include <stddef.h>
+
+const char* zopfli_compress(size_t input_size, const char* input, size_t* output_size) {
+    *output_size = 0;
+    return 0;
+}
