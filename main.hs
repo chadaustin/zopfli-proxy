caa86f67fbdc1d35daa197f11540c03e6bdc9881
Chad Austin <caustin@gmail.com>
2014-02-12 01:05:06 -0800

    request and response translation

--- main.hs
+++ main.hs
@@ -10,32 +10,41 @@ import Foreign.Marshal.Alloc
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import qualified Data.ByteString.Char8 as BS
 import qualified Network.Wai as Wai
-import Network.HTTP
 import qualified Network.HTTP.Conduit as HC
-import Network.HTTP.Base
 import qualified Network.HTTP.Types as HT
-import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 
 foreign import ccall "zopfli_compress" zopfli_compress
   :: CSize -> CString -> Ptr CSize -> CString
 
-app config request = do
-  let method = Wai.requestMethod request
-  let backendPath = Wai.rawPathInfo request
-  backendRequest <- HC.parseUrl $ config <> (BS.unpack backendPath)
-  putStrLn $ show backendRequest
+-- zopfli compression should be middleware
+-- resp <- alloca $ \output_size -> return $ BSL.pack $ show $ zopfli_compress (CSize 0) nullPtr output_size
+
+convertResponse response =
+  Wai.responseLBS
+    (HC.responseStatus response)
+    (HC.responseHeaders response)
+    (HC.responseBody response)
+
+app request = do
   -- TODO: disallow non-GET-or-HEAD
-  response <- HC.withManager $ \manager ->
-    HC.httpLbs backendRequest manager
+  --let method = Wai.requestMethod request
+  let backendPath = Wai.rawPathInfo request
+  response <- HC.withManager $ \manager -> do
+    requestp <- HC.parseUrl $ "http://localhost:8000" <> (BS.unpack backendPath)
+    requestBody <- liftIO $ Wai.lazyRequestBody request
+    let backendRequest = requestp {
+          HC.method = Wai.requestMethod request,
+          HC.requestHeaders = Wai.requestHeaders request,
+          HC.requestBody = HC.RequestBodyLBS requestBody,
+          HC.checkStatus = \_r _s _h -> Nothing }
+                                        
+    HC.httpLbs backendRequest {HC.checkStatus = \_r _s _h -> Nothing} manager
   --backend <- HC.httpLbs backendRequest manager -- simpleHTTP (getRequest "http://localhost:4000")
   
-  resp <- alloca $ \output_size -> return $ BSL.pack $ show $ zopfli_compress (CSize 0) nullPtr output_size
-  return $ Wai.responseLBS HT.status200 [] $ HC.responseBody response
+  return $ convertResponse response
 
 main :: IO ()
 main = do
-  let config = "http://localhost:8000"
-  Warp.run 3000 $ app config
-
-  return ()
+  --config <- HC.parseUrl $ "http://localhost:8000"
+  Warp.run 3000 $ app
